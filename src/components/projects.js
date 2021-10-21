0d8ed3d8f6b5e9021a2f89f765f7b44c871f6648
Henry <hhpendleton@gmail.com>
2021-10-21 17:29:17 -0400

    I need spell check

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,7 +5,7 @@ export const data = {
       title: "The Watch List",
       specs: ["Vanilla JavaScript", "HTML", "CSS", "REST API", "SPA"],
       description:
-        "This was the first project in my Flatiron Software Engineer Program.  'The Watch List' is a vanilla javascript sigle page application.  In the webpage we pull from a movie database where we can dynamically search and filter for movies.  Once you found a movie you would to watch you can save it to our JSON server backend.  On your watch list you have the ability to toggle if you have watched the movie and if so, you can rate the movie 1-10.",
+        "This was the first project in my Flatiron Software Engineer Program. 'The Watch List' is a vanilla javascript single-page application. In the webpage, we pull from a movie database where we can dynamically search and filter for movies. Once you find a movie you would to watch you can save it to our JSON server backend. On your watch list you have the ability to toggle if you have watched the movie and if so, you can rate the movie 1-10.",
       url: "https://github.com/Hank95/Phase-1-Project",
     },
     {
@@ -13,7 +13,7 @@ export const data = {
       title: "Pictogram",
       specs: ["React Hooks", "Express", "MySQL"],
       description:
-        "Pictogram was the second project in my Flatiron program.  The goal of this one was to use React to create a REST api web app.  Pictogram mixes instagram and MS Paint.  Users have the ability to sketch there own drawings and then post them to the world.  The sketch pad is done with the Canvas API and then posted to a MySQL backend.  ",
+        "Pictogram was the second project in my Flatiron program. The goal of this one was to use React to create a REST API web app. Pictogram mixes Instagram and MS Paint. Users have the ability to sketch their drawings and then post them to the world. The sketch pad is done with the Canvas API and then posted to a MySQL backend. ",
       url: "https://github.com/Hank95/pictogram-client",
     },
     {
@@ -28,7 +28,7 @@ export const data = {
         "LeafletJS",
       ],
       description:
-        "The Bar Finder was my first app using Ruby to creat my own API.  The app is designed for users to search there local town to find bars and pubs with bar games that they want to play.  A user can add and delete bars to the site.  I used the LeafletJS API to add mapping funtionality so a user can browse based on location. ",
+        "The Bar Finder was my first app using Ruby to create my own API. The app is designed for users to search their local town to find bars and pubs with bar games that they want to play. A user can add and delete bars to the site. I used the LeafletJS API to add mapping functionality so a user can browse based on location. ",
       url: "https://github.com/Hank95/Phase-3-project-client",
       youtube: "kx4etfuPt_A",
     },
@@ -37,7 +37,7 @@ export const data = {
       title: "Laurel Bike Shop",
       specs: ["React", "Rails", "User Auth", "PostgresQL", "E Commerce"],
       description:
-        "The Laurel Bike Shop is a full stack ecommerce web app built with a React front end, a Ruby on Rails API and PostgresQL database.  The site has a session token based user authentication for users to create accounts, and save there shopping cart for a later purchase.  This site has full CRUD actions where user create, read, update, and delete there shopping cart.  For browsing, I implimanted pagination to allow for speedy rendering and a better user experience.",
+        "The Laurel Bike Shop is a full-stack eCommerce web app built with a React front end, a Ruby on Rails API, and PostgreSQL database. The site has a session token-based user authentication for users to create accounts and save their shopping cart for a later purchase. This site has full CRUD actions where users create, read, update, and delete their shopping cart. For browsing, I implemented pagination to allow for speedy rendering and a better user experience.",
       url: "https://github.com/Hank95/phase-4-project",
       youtube: "jva52zQTH5s",
     },
@@ -53,7 +53,7 @@ export const data = {
         "AWS S3",
       ],
       description:
-        "AHOY was my Capstone project for the Flatiron School Software Engineer Program. It is a peer-to-peer boat rental application, built with a React frontend, a Rubby on Rails API, and a PostgresQL database.  I used AWS S3 for data storage and the Google Maps API for mapping and geocoding.  In this app users can log in to there account, browse and book, and review boats as well as post there own boats to the site.",
+        "AHOY was my Capstone project for the Flatiron School Software Engineer Program. It is a peer-to-peer boat rental application, built with a React frontend, a Ruby on Rails API, and a PostgreSQL database. I used AWS S3 for data storage and the Google Maps API for mapping and geocoding. In this app users can log in to their account, browse and book, and review boats as well as post their own boats to the site.",
       url: "https://github.com/Hank95/capstone-project",
       youtube: "RFWkbkm5nu4",
     },
